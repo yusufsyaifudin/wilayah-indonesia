40639c6c269d72de7bbe1e7e1d5f011d91732c84
Muhammad Rifqi Fatchurrahman <muh_rif@live.com>
2020-10-27 19:56:24 +0700

    add lombok

--- wilayah-indonesia/src/main/java/wilayah/indonesia/model/Regency.java
+++ wilayah-indonesia/src/main/java/wilayah/indonesia/model/Regency.java
@@ -1,11 +1,14 @@
 package wilayah.indonesia.model;
 
-import java.util.ArrayList;
 import java.util.List;
+import lombok.AllArgsConstructor;
+import lombok.Data;
 
 /**
  * Created by yusuf on 24/10/16.
  */
+@Data
+@AllArgsConstructor
 public class Regency {
 
     private String id;
@@ -15,51 +18,4 @@ public class Regency {
     private Double latitude;
     private Double longitude;
     private List<District> districts;
-
-    public String getId() { return id; }
-
-    public void setId(String id) { this.id = id; }
-
-    public String getProvince_id() { return province_id; }
-
-    public void setProvince_id(String province_id) { this.province_id = province_id; }
-
-    public String getName() { return name; }
-
-    public void setName(String name) { this.name = name; }
-
-    public String getAlt_name() { return alt_name; }
-
-    public void setAlt_name(String alt_name) { this.alt_name = alt_name; }
-
-    public Double getLatitude() {
-        return (latitude == null) ? 0 : latitude;
-    }
-
-    public void setLatitude(Double latitude) { this.latitude = latitude; }
-
-    public Double getLongitude() {
-        return (longitude == null) ? 0 : longitude;
-    }
-
-    public void setLongitude(Double longitude) { this.longitude = longitude; }
-
-    public List<District> getDistricts() {
-        return (districts != null) ? districts : new ArrayList<>();
-    }
-
-    public void setDistricts(List<District> districts) {
-        this.districts = districts;
-    }
-
-
-    public String toString() {
-        return "{id: " + getId().toString() +
-                ", province_id: " + getProvince_id().toString() +
-                ", name: " + getName() +
-                ", alt_name: " + getAlt_name() +
-                ", latitude: " + getLatitude().toString() +
-                ", longitute: " + getLongitude().toString() +
-                "}";
-    }
 }
